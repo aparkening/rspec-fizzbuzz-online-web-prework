193c36e416940d3e6167c714ebfd70f2bf3a0973
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-19 17:21:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-fizzbuzz returns "Fizz" when the number is divisible by 3
-     Failure/Error: fizz_3 = fizzbuzz(3)
+def fizzbuzz(number) 
+  
+end
+
